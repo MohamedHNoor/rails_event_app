3f820222c0707e116d3f6007f33953e2f68a1390
Mohamed Noor <mohmed.hassannoor114@gmail.com>
2025-12-15 23:00:41 +1300

    Add signin session method and signout session method with the form template and button link for signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+   user = User.find_by(email: params[:email])
+   if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect_to (session[:intended_url] || user), notice: "Welcome back #{user.name} 👋"
+     session[:intended_url] = nil
+   else
+    flash.now[:alert] = "🛑 Invalid email or password"
+    render :new, status: :unprocessable_content
+   end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to events_url, status: :see_other,  notice: "You signed out!"
   end
 end
