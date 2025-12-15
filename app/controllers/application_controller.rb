679b2bf57233b55f2d40f53670608372c50314e8
Mohamed Noor <mohmed.hassannoor114@gmail.com>
2025-12-15 22:58:12 +1300

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :current_user?
+  helper_method :current_user_admin?
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  def require_signin
+    unless current_user
+      session[:intended_url] = request.url
+      redirect_to new_session_url, notice: "Please signin"
+    end
+  end
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to events_url, alert: "Unauthorized access 🛑"
+    end
+  end
+  
 end
